e6b7dc23123d331779be5847a86f0b442cce4253
George Fisher <george@georgefisher.com>
2014-07-09 16:46:37 -0500

    sample credentials file

---
+++ twitter_credentials.py
@@ -0,0 +1,12 @@
+def twitter_credentials():
+    """
+    This file is called by the Twitter functions to identify
+    you as an authorized developer.
+    
+    Fill in your information and save in the same folder as the other python programs
+    """
+    api_key = " your key "
+    api_secret = " your secret "
+    access_token_key = " your token key "
+    access_token_secret = " your token secret "
+    return (api_key,api_secret,access_token_key,access_token_secret)
