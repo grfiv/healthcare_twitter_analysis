b29351ccf23439f3fbe7a255c78307f28edb0ea3
George Fisher <george@georgefisher.com>
2014-07-25 11:46:11 -0400

    updated Twitter json retrieval

--- README.md
+++ README.md
@@ -22,40 +22,49 @@ The 897 files for this project are located on Google Drive. Install the app and 
 
 However, these files have none of the Twitter data besides the ['text'] field (which is called "content").  
 
-The repo ([GitHub](https://github.com/grfiv/healthcare_twitter_analysis)) contains two python programs, in the `code` folder, to solve this problem:  
+The repo ([GitHub](https://github.com/grfiv/healthcare_twitter_analysis)) contains two python programs in the `code` folder to solve this problem:  
 Both read in a text file containing a list of fully-qualified file names to process ...    
-1. `create_jsonfile.py` produces a json output file with the exact Twitter json for each tweet.   
+
+1. `get_twitter_json.py` produces a json output file with the exact Twitter json for each tweet, augmented by additional fields as shown in `fields_added_to_twitter_json.txt` in the `files` folder.   
 2. `create_bulkfile.py` produces a csv output file with additional Twitter data fields plus calculated fields like sentiment from AFINN-111 and place name & coordinates from the Google Geocoding API. See `list_of_variable_names_in_the_processed_file.txt` in the `files` folder for a list of columns.  
 
-Samples of both file types are included in the `files` folder.
+Samples of both file types are included in the `files` folder. 
 
-I have used both of these programs to create files of subsets of the data ... all the files referencing Endocrine, for example, to do research specifically on that disease category. See the `analyses` folder.  
-
-**create_jsonfile.py** 
-```  
-def create_jsonfile(list_of_filenames, starting_at=1, ending_at=0):
+**get_twitter_json.py**   
+```     
+def get_twitter_json(list_of_filenames, starting_at=1, ending_at=0, geocode=True):
     """
-    - reads in a list of fully-qualified filenames from "list_of_filenames"
+    1) reads in a list of fully-qualified filenames from "list_of_filenames"
         
-    - processes each row of each file in the file list, 
-      making batched calls to Twitter to retrieve the data for each tweet
+    2) processes each row of each file in the file list, 
+       making batched calls to Twitter to retrieve the json for each tweet
+           adding the data from topsy file such as "score"
+           plus unix timestamps for the topsy firstpost_date field and Twitter's created_at field
+           plus coordinate and place name data for Twitter's user location field
     
     - after every 13,500 rows, or whenever there is a threshold-exceeded error
-      the output_file is written and the program goes to sleep for 15 minutes.
+      the program goes to sleep for 15 minutes.
       
     Input: list_of_filenames   a text file with fully-qualified file names
            starting_at         the line number of "list_of_filenames" where processing should start
            ending_at           if 0   process all files beginning with the "starting_at" line in "list_of_filenames"
                                if > 0 process the files from line "starting_at" to line "ending_at" in "list_of_filenames"
+           geocode             if True, calls are made to the MapQuest Open Nominatim Search Service for coordinate and place name data
+                               if False, these call are not made
+                                 this function dramatically slows down the overall process; if you don't need
+                                 location info, you'll be happier with geocode=False
            
     Output: a text file named "bigtweet_filexxx.json", where xxx is the "starting_at" number
         
-    Usage: %run create_jsonfile.py "filename_list.csv" 1 0
+    Usage: %run get_twitter_json.py "filename_list.csv" 1 0
+    
+    A message like "6 skipped id 448176144668721152" means that Twitter failed to return any data about 
+    a tweet with id 448... and that this is the 6th instance of this.
     
     To use the output file in python:
     =================================
 import json
-tweet_file = open("../files/bigtweet_file003.json", "r")
+tweet_file = open("../files/bigtweet_file002.json", "r")
 for line in tweet_file:
     tweet = json.loads(str(line))
     if tweet['retweet_count'] > 100:
@@ -65,7 +74,7 @@ for line in tweet_file:
     To use the output file in R:
     ============================
 library(rjson)
-file_path  = ("../files/bigtweet_file003.json")
+file_path  = ("../files/bigtweet_file002.json")
 tweet_list = fromJSON(sprintf("[%s]", paste(readLines(file_path),collapse=",")))
 
 for (i in 1:length(tweet_list)){
@@ -81,13 +90,13 @@ tweets = import_statuses(raw_data=tweet_list)
    =================================
 # create a python list of each tweet
 import json
-tweet_file = open("../files/bigtweet_file003.json", "r")
+tweet_file = open("../files/bigtweet_file002.json", "r")
 tweet_list = [json.loads(str(line)) for line in tweet_file]
 
 # store the list in MongoDB
 from pymongo import MongoClient
 client = MongoClient()
-db     = client['file003']
+db     = client['file002']
 posts  = db.posts
 #db.posts.remove( { } ) # delete if previously created
 
@@ -120,7 +129,9 @@ def create_bulkfile(list_of_filenames, starting_at=1, ending_at=0):
       
     Note: AFINN-111.txt must be in the same folder
           you can use it as is or include your own n-grams
-          the 'sentiment' field is the sum of the scores of all the n-grams found
+          the 'sentiment' field is the sum of the scores of all the n-grams found  
+          
+    Note: Requires pygeocoder  
     
     Input: list_of_filenames   a text file with fully-qualified file names
            starting_at         the line number of "list_of_filenames" where processing should start
@@ -202,4 +213,4 @@ The dataset consists of ~2.5 Million tweets, more than 15 Million words. Potenti
 
 3. Business/ Domain Research: What kind of data is readily available (example - open government data) that could be useful to correlate with disease related tweets. Can we find a collection of hospital names, drug names, important medical hashtags, twitter usernames of healthcare organizations etc. that will aid in this project. What are the business use cases for this data set?     
 
-4. Visualization Research: Finally one of the most critical, what set of visualization would help describe this data? Which libraries / tools could be used? I would recommend everyone to visit the ìCollaboration Spaceî on Coursolve where we are having many interesting discussions. You would get a lot more ideas and probably collaborate on the next steps too.   
+4. Visualization Research: Finally one of the most critical, what set of visualization would help describe this data? Which libraries / tools could be used? I would recommend everyone to visit the ‚ÄúCollaboration Space‚Äù on Coursolve where we are having many interesting discussions. You would get a lot more ideas and probably collaborate on the next steps too.   
--- code/get_twitter_json.py
+++ code/get_twitter_json.py
@@ -1,26 +1,36 @@
-def create_jsonfile(list_of_filenames, starting_at=1, ending_at=0):
+def get_twitter_json(list_of_filenames, starting_at=1, ending_at=0, geocode=True):
     """
-    - reads in a list of fully-qualified filenames from "list_of_filenames"
+    1) reads in a list of fully-qualified filenames from "list_of_filenames"
         
-    - processes each row of each file in the file list, 
-      making batched calls to Twitter to retrieve the data for each tweet
+    2) processes each row of each file in the file list, 
+       making batched calls to Twitter to retrieve the json for each tweet
+           adding the data from topsy file such as "score"
+           plus unix timestamps for the topsy firstpost_date field and Twitter's created_at field
+           plus coordinate and place name data for Twitter's user location field
     
     - after every 13,500 rows, or whenever there is a threshold-exceeded error
-      the output_file is written and the program goes to sleep for 15 minutes.
+      the program goes to sleep for 15 minutes.
       
     Input: list_of_filenames   a text file with fully-qualified file names
            starting_at         the line number of "list_of_filenames" where processing should start
            ending_at           if 0   process all files beginning with the "starting_at" line in "list_of_filenames"
                                if > 0 process the files from line "starting_at" to line "ending_at" in "list_of_filenames"
+           geocode             if True, calls are made to the MapQuest Open Nominatim Search Service for coordinate and place name data
+                               if False, these call are not made
+                                 this function dramatically slows down the overall process; if you don't need
+                                 location info, you'll be happier with geocode=False
            
     Output: a text file named "bigtweet_filexxx.json", where xxx is the "starting_at" number
         
-    Usage: %run create_jsonfile.py "filename_list.csv" 1 0
+    Usage: %run get_twitter_json.py "filename_list.csv" 1 0
+    
+    A message like "6 skipped id 448176144668721152" means that Twitter failed to return any data about 
+    a tweet with id 448... and that this is the 6th instance of this.
     
     To use the output file in python:
     =================================
 import json
-tweet_file = open("../files/bigtweet_file003.json", "r")
+tweet_file = open("../files/bigtweet_file002.json", "r")
 for line in tweet_file:
     tweet = json.loads(str(line))
     if tweet['retweet_count'] > 100:
@@ -30,7 +40,7 @@ for line in tweet_file:
     To use the output file in R:
     ============================
 library(rjson)
-file_path  = ("../files/bigtweet_file003.json")
+file_path  = ("../files/bigtweet_file002.json")
 tweet_list = fromJSON(sprintf("[%s]", paste(readLines(file_path),collapse=",")))
 
 for (i in 1:length(tweet_list)){
@@ -46,13 +56,13 @@ tweets = import_statuses(raw_data=tweet_list)
    =================================
 # create a python list of each tweet
 import json
-tweet_file = open("../files/bigtweet_file003.json", "r")
+tweet_file = open("../files/bigtweet_file002.json", "r")
 tweet_list = [json.loads(str(line)) for line in tweet_file]
 
 # store the list in MongoDB
 from pymongo import MongoClient
 client = MongoClient()
-db     = client['file003']
+db     = client['file002']
 posts  = db.posts
 #db.posts.remove( { } ) # delete if previously created
 
@@ -62,12 +72,11 @@ posts.insert(tweet_list)
 for result in db.posts.find({ "retweet_count": { "$gt": 100 } }):
     print "%d %s\n%s"%(result['retweet_count'],result['user']['name'],result['text'])
     """
-    import csv
-    import json
+    import csv, json
     import re
-    import time
-    import sys
-    import datetime
+    import time, datetime
+    import sys, os
+    import urllib2
     from twitter_functions import lookup_multiple_tweets
     
     # convert input parameter strings to integer
@@ -78,6 +87,7 @@ for result in db.posts.find({ "retweet_count": { "$gt": 100 } }):
     print "\n================================"
     print "process start: %s"%process_start.strftime("%c")
     print "================================\n"
+    sys.stdout.flush()
     
     # read the list of filenames into "filename_list"
     # ===============================================
@@ -89,6 +99,7 @@ for result in db.posts.find({ "retweet_count": { "$gt": 100 } }):
     
     output_filename   = "bigtweet_file" + "%03d"%(starting_at,) + ".json"
     step              = 100 # we're going to process in groups of "step"
+    bulk_list         = []  # batch of rows from input file
     list_of_tweet_ids = []  # tweet ids of these rows
     output_dict       = []  # list of dicts to send to output file
     
@@ -96,6 +107,9 @@ for result in db.posts.find({ "retweet_count": { "$gt": 100 } }):
     # https://dev.twitter.com/docs/rate-limiting/1.1/limits
     sleep_batch       = 13500 # we sleep after this many lines processed
     sleep_batch_rows  = 0     # the number of lines we've processes since the last sleep
+    Geocoder_count    = 0     # how many records did not get Geocoding
+    # MapQuest Nominatim documentation: http://open.mapquestapi.com/nominatim/
+    mapq_url          = 'http://open.mapquestapi.com/nominatim/v1/search.php?format=json&limit=1&addressdetails=0&json_callback=renderBasicSearchNarrative&q='
     
     number_of_files   = len(filename_list) # how many files in the list
     file_counter      = 1                  # which one is this one
@@ -104,6 +118,7 @@ for result in db.posts.find({ "retweet_count": { "$gt": 100 } }):
     invalid_json      = False              # in case Twitter sends us junk
     global total_processed
     total_processed   = 0                  # how many rows have we processed
+    skip_counter      = 0                  # how many rows did we skip because Twitter didn't send us info
     
     # read each file in and process it
     # ==================================
@@ -140,6 +155,7 @@ for result in db.posts.find({ "retweet_count": { "$gt": 100 } }):
             totallines = len(lines)   # number of lines in the input file
             
             print "\n--Processing %d of %d %s rows %d"%(file_counter, number_of_files, short_file_name,totallines)
+            sys.stdout.flush()
             
             # read the input file line-by-line
             # ================================
@@ -159,6 +175,7 @@ for result in db.posts.find({ "retweet_count": { "$gt": 100 } }):
                 if re.match(r"^\d+", tweet_id):
                     # Successful match at the start of the string
                     row['id'] = tweet_id
+                    bulk_list.append(row)
                     list_of_tweet_ids.append(tweet_id)
                 else:
                     print "tweet url terminated with non-numeric in line %d"%(linenum+1)
@@ -181,6 +198,7 @@ for result in db.posts.find({ "retweet_count": { "$gt": 100 } }):
                             print "\nTwitter returned invalid json"
                             print e
                             print "after %d lines of file %d of %d %s"%(linenum, file_counter, number_of_files, short_file_name)
+                            bulk_list = []
                             invalid_json = True
                             break
                             
@@ -189,13 +207,6 @@ for result in db.posts.find({ "retweet_count": { "$gt": 100 } }):
                         break
                         
                     # if Twitter returns an error
-                    #
-                    # better process
-                    # try:
-                    #     statuses = api.GetUserTimeline(u.id)
-                    #     print [s.text for s in statuses]
-                    # except TwitterError, t:
-                    #     print t
                     if 'errors' in tweetdata_list:
                         print "Twitter returned an error message:"
                         print "message: " + str(tweetdata_list["errors"][0]['message'])
@@ -203,9 +214,106 @@ for result in db.posts.find({ "retweet_count": { "$gt": 100 } }):
                         print "after %d lines of file %d of %d %s"%(linenum, file_counter, number_of_files, short_file_name)
                         sleep_batch_rows = 0
                         sleep_process(tweetdata_list, output_filename)
+                        bulk_list = [] # we lose the batch
                         continue
+                    
+                    # Twitter's response is in arbitrary order and doesn't necessarily
+                    # contain a response for every id we requested
+                    #
+                    # So we create a dictionary for the tweetdata_list
+                    # associating id's with their position in the list
+                    # and a list of id's for searching
+                    
+                    tweet_id_dict = {}
+                    tweet_id_list = []
+                    
+                    # save every id in tweetdata_list and its position
+                    for i in range(len(tweetdata_list)):
+                        id = str(tweetdata_list[i]['id'])
+                        tweet_id_dict[id] = i
+                        tweet_id_list.append(id)
                         
+                    # pull each of the lines and its corresponding Twitter response
+                    for line in bulk_list:
+                        if line['id'] not in tweet_id_list:
+                            skip_counter+=1
+                            # check the entire line['id'] is numeric
+                            if re.match(r"^\d+", line['id']):
+                                # yes
+                                print "%d skipped id %d"%(skip_counter, int(line['id']))
+                            else:
+                                # no
+                                print skip_counter
+                                print "line['id'] is not all numeric"
+                                print line['id']                               
+                            continue
+                            
+                        tweetdata = tweetdata_list[tweet_id_dict[line['id']]]
+                        if str(line['id']) != str(tweetdata['id']):
+                            skip_counter+=1
+                            print "id mismatch, skipping %d"%(skip_counter)
+                            print "line  id %s"%(str(line['id']))
+                            print "tweet id %s"%(str(tweetdata['id']))
+                            continue
+                            
+                        # =========================================
+                        # add new fields to Twitter's json response
+                        # =========================================
+                            
+                        # add a timestamp for 'created_at'
+                        # time.ctime(tweet['timestamp']) # will decode this field
+                        tweetdata_list[tweet_id_dict[line['id']]]['timestamp'] = \
+                            time.mktime(datetime.datetime.strptime(tweetdata_list[tweet_id_dict[line['id']]]['created_at'], '%a %b %d %H:%M:%S +0000 %Y').timetuple())
+                            
+                        tweetdata_list[tweet_id_dict[line['id']]]['topsy'] = {}
+                        # add a timestamp for topsy's 'firstpost_date' 
+                        try: 
+                            tweetdata_list[tweet_id_dict[line['id']]]['topsy']['timestamp'] = \
+                                time.mktime(datetime.datetime.strptime(line['firstpost_date'], "%m/%d/%Y").timetuple())
+                        except:
+                            try:
+                                tweetdata_list[tweet_id_dict[line['id']]]['topsy']['timestamp'] = \
+                                    time.mktime(datetime.datetime.strptime(line['firstpost_date'], "%m/%d/%y").timetuple())
+                            except:
+                                tweetdata_list[tweet_id_dict[line['id']]]['topsy']['timestamp'] = ""
+                                
+                        # add the topsy csv file fields to the Twitter json
+                        tweetdata_list[tweet_id_dict[line['id']]]['topsy']['firstpost_date']        = line['firstpost_date']
+                        tweetdata_list[tweet_id_dict[line['id']]]['topsy']['score']                 = float(line['score'])
+                        tweetdata_list[tweet_id_dict[line['id']]]['topsy']['trackback_author_nick'] = line['trackback_author_nick']
+                        tweetdata_list[tweet_id_dict[line['id']]]['topsy']['trackback_author_url']  = line['trackback_author_url']
+                        tweetdata_list[tweet_id_dict[line['id']]]['topsy']['trackback_permalink']   = line['trackback_permalink']
+                        tweetdata_list[tweet_id_dict[line['id']]]['topsy']['url']                   = line['url']
+                        
+                        tweetdata_list[tweet_id_dict[line['id']]]['topsy']['file_counter']          = file_counter
+                        tweetdata_list[tweet_id_dict[line['id']]]['topsy']['short_file_name']       = short_file_name
+                        
+                        # add coordinates & place name for user location
+                        if geocode:
+                            tweetdata_list[tweet_id_dict[line['id']]]["user"]["location_geoinfo"] = {}
+                            if tweetdata_list[tweet_id_dict[line['id']]]["user"]["location"] is not None:
+                                location = tweetdata_list[tweet_id_dict[line['id']]]["user"]["location"]
+                                location  = re.sub(r"[^\w\s]", " ", location) # remove punctuation
+                                location  = re.sub("[ ]{1,}", "+", location)  # replace blanks with +
+                                response  = urllib2.urlopen(mapq_url + location)
+                                mapq_resp = response.read()
+                                loc_json = ''
+                                urls = re.findall("{.*?}", mapq_resp)
+                                if urls: 
+                                    try:
+                                        loc_json = json.loads(urls[0])
+                                        tweetdata_list[tweet_id_dict[line['id']]]["user"]["location_geoinfo"] = loc_json
+                                        Geocoder_count += 1
+                                    except Exception as e:
+                                        print "location json exception"
+                                        print urls
+                                        print sys.exc_info()
+                                
+                        
+                    # process the file and start over
+                    # ===============================
                     process_output_file(tweetdata_list, output_filename)
+                    bulk_list = []
                   
         file_counter+=1
                             
@@ -220,6 +328,8 @@ for result in db.posts.find({ "retweet_count": { "$gt": 100 } }):
     print "process start: %s"%process_start.strftime("%c")
     print "process end:   %s"%process_end.strftime("%c")
     print "process elapsed hours %0.2f"%process_hours
+    if Geocoder_count > 0:
+        print "%d records geo-encoded"%Geocoder_count
     print "================================\n"    
     
                     
@@ -243,30 +353,36 @@ def sleep_process(output_dict, output_filename):
 def process_output_file(output_dict, output_filename):
     import json
     import datetime
+    import io
+    import sys
     
     global total_processed
     global first_sleep
     
     if 'errors' not in output_dict:
         if first_sleep:
-            with open(output_filename, 'wb') as f:
+            with io.open(output_filename, 'w', encoding='utf-8') as f:
                 first_sleep = False
                 for tweet in output_dict:
-                    json.dump(tweet,f)
-                    f.write("\n")
+                    f.write(unicode(json.dumps(tweet, ensure_ascii=False)))
+                    f.write(u"\n")
                     total_processed+=1
         else:
-            with open(output_filename, 'a') as f:
+            with io.open(output_filename, 'a', encoding='utf-8') as f:
                 for tweet in output_dict:
-                    json.dump(tweet,f)
-                    f.write("\n")
+                    f.write(unicode(json.dumps(tweet, ensure_ascii=False)))
+                    f.write(u"\n")
                     total_processed+=1
                 
         timenow     = datetime.datetime.today().strftime("%H:%M:%S")
         print "%s processed at %s, rows %d"%(output_filename, timenow, total_processed)
+        sys.stdout.flush()
     output_dict = []
     
                     
 if __name__ == '__main__':
     import sys
-    create_jsonfile(sys.argv[1],sys.argv[2],sys.argv[3])
+    if len(sys.argv) == 2: get_twitter_json(sys.argv[1],starting_at=1, ending_at=0, geocode=True)
+    if len(sys.argv) == 3: get_twitter_json(sys.argv[1],sys.argv[2], ending_at=0, geocode=True)
+    if len(sys.argv) == 4: get_twitter_json(sys.argv[1],sys.argv[2], sys.argv[3], geocode=True)
+    if len(sys.argv) == 5: get_twitter_json(sys.argv[1],sys.argv[2], sys.argv[3], sys.argv[4])
